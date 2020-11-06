e43683f4e992db4989bf157783640aca3ed03b88
Tori Colthurst <vrc2@illinois.edu>
2020-11-06 14:47:56 -0600

    might override Dhruv tbh

--- Musical_Cues/video_processing.py
+++ Musical_Cues/video_processing.py
@@ -21,16 +21,23 @@ def read_video(video_file_path):
         ret, frame = cap.read()
         if ret == True:
             # display frame
-            # cv2.imshow("Frame", frame)
+            cv2.imshow("Frame", frame)
 
             # press Q on keyboard to exit
-            # if cv2.waitKey(25) & 0xFF == ord('q'):
-            #     break
-            
-            video_frames.append(frame)
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+
+            # video_frames.append(frame)
         else:
+            print("Nothing read. Exiting read video loop.")
             break
 
     # release video capture object and close all frames
     cap.release()
     cv2.destroyAllWindows()
+
+def main():
+    read_video("Footage/Theo_540_w_Audio.mp4")
+
+if __name__ == "__main__":
+    main()
