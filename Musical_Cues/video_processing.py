c1ca817eacf5ed2b57b9205c86170f9b9bcaef04
Tori Colthurst <vrc2@illinois.edu>
2020-11-06 14:11:44 -0600

    starter

---
+++ Musical_Cues/video_processing.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2
+
+'''
+read_video function
+description:
+inputs:
+outputs: -1, error
+'''
+def read_video(video_file_path):
+
+    cap = cv2.VideoCapture(video_file_path)
+
+    if (cap.isOpened()== False):
+        print("ERROR: failed to open video file.")
+        return -1
+
+    # read video frame by frame
+    video_frames = []
+    while (cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+            # display frame
+            # cv2.imshow("Frame", frame)
+
+            # press Q on keyboard to exit
+            # if cv2.waitKey(25) & 0xFF == ord('q'):
+            #     break
+            
+            video_frames.append(frame)
+        else:
+            break
+
+    # release video capture object and close all frames
+    cap.release()
+    cv2.destroyAllWindows()
