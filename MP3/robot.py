6b756718bcb2564c5c06857b95ed962c5485f98d
Tori Colthurst <vrc2@illinois.edu>
2020-11-23 20:56:33 -0600

    kind of functional

--- MP3/robot.py
+++ MP3/robot.py
@@ -1,4 +1,4 @@
-import numpy as np
+import math
 
 ''' Notes:
 
@@ -18,82 +18,63 @@ self.lander.angle,
 
 class robot:
     def __init__(self):
-        # self.all_state = {1:"Start", 2:"Tipping Left", 3: "Tipping Right"}
-        # curr_state = 1
-        self.toggle = [0.5, 0.0, 0.0]
-        # self.balance = 0
-        self.throttle = 0
-        # self.j = 0
+        self.x = 0.0
+        self.y = 1.0
 
     # clockwise = negative
     # counterclockwise = positive
     # First controls main engine, -1..0 off, 0..+1 throttle from 50% to 100% power.
     # Second value -1.0..-0.5 fire left engine, +0.5..+1.0 fire right engine, -0.5..0.5 off.
     def update(self, status, user_action):
-        angular_vel = status[5]
-        angle = status[4]
-        # y_vel = status[]
-        action = [0.0, 0.0]
 
-        print(angle)
-
-        if abs(angle) <= 0.3:
-            if user_action == "left":
-                action = [0.5, -1.0]
-            elif user_action == "right":
-                action = [0.5, 1.0]
-            elif user_action == "up":
-                action = [1.0, 0.0]
-            elif user_action == "down":
-                action = [0.0, 0.0]
-        elif angle < -0.3:
-            action = [0.5+abs(1/angle), -0.75+angle]
-        elif angle > 0.3:
-            action = [0.5+abs(1/angle), 0.75+angle]
-        #
-        # if not self.balance:
-        #     if not self.throttle:
-        #         action = [0.5, angular_vel*15]
-        #     else:
-        #         action = [0.0, angular_vel*15]
-        # else:
-        #     if user_action == "left":
-        #         action = [0.5, -1.0]
-        #     elif user_action == "right":
-        #         action = [0.5, 1.0]
-        #     elif user_action == "up":
-        #         action = [0.75, angular_vel*15]
-        #     elif user_action == "down":
-        #         action = [0.0, angular_vel*15]
-        #
-        # self.balance = (self.balance + 1) % 5
-        self.throttle = (self.throttle + 1) % 2
-
-        return self.balance(status)
-
-    # x = 0 center
-    # y = 1.5 top
-    def balance(self, status):
-        x_pos = status[0]
-        y_pos = status[1]
+        x0 = status[0]
+        y0 = status[1]
         x_vel = status[2]
         y_vel = status[3]
         angle = status[4]
         angular_vel = status[5]
 
-        action = [0.0, 0.0]
-        print("(x,y): ("+str(x_pos)+", "+str(y_pos)+")")
+        x_target = self.x
+        y_target = self.y
 
-        if y_pos < 1.0:
-            action[0] = 0.5 + (1.0-y_pos)
-        else:
-            action[0] = 0.0
+        if user_action == "left": # positive
+            x_target = self.x - 0.01
+            y_target = self.y
+        elif user_action == "right":
+            x_target = self.x + 0.01
+            y_target = self.y
+        elif user_action == "up":
+            x_target = self.x
+            y_target = self.y + 0.01
+        elif user_action == "down":
+            x_target = self.x
+            y_target = self.y - 0.01
 
-        if x_pos < 0.0:
-            action[1] = 0.5 + abs(x_pos)
-        else:
-            action[1] = -0.5 - abs(x_pos)
+        self.x = x_target
+        self.y = y_target
 
-        # action[1] = angular_vel*20
+        b = self.balance(angle, angular_vel, y0, y_vel, x0, x_vel, x_target, y_target)
+        return b
+
+    # x = 0 center
+    # y = 1.5 top
+    def balance(self, ang, ang_v, y, y_vel, x, x_vel, x_target, y_target):
+        # p = p_0 + v_0*t + 0.5*a*t^2
+        # a = (p - p_0 - v_0) / (0.5)
+
+        t = 0.5
+        scale = 1.0
+
+        a = (0.0 - (ang + ang_v*t)) / (0.5*t**2)
+        x_acc = (x_target - (x + x_vel*t)) / (0.5*t**2)
+        y_acc = (y_target - (y + y_vel*t)) / (0.5*t**2)
+
+        if abs(ang) > math.pi/16:
+            scale = 0.5
+
+        left_right_engine = -a + x_acc
+        main_engine = y_acc * scale
+
+        action = [main_engine, left_right_engine]
 
         return action
